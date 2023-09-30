d76893252d43dfd845d524236ffa04f279f64a40
StandHereHermawan <nolifegansaya@gmail.com>
2023-09-30 10:01:52 +0700

    Register User Api, add repository interfaces

---
+++ src/main/java/ariefbelajarteknologi/restful/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package ariefbelajarteknologi.restful.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ariefbelajarteknologi.restful.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+}
