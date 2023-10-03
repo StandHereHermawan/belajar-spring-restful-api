fb169ab7837b4166ce6f1968297de4b110612591
StandHereHermawan <nolifegansaya@gmail.com>
2023-10-03 09:10:15 +0700

    Get User API, modify controller class

--- src/main/java/ariefbelajarteknologi/restful/controller/UserController.java
+++ src/main/java/ariefbelajarteknologi/restful/controller/UserController.java
@@ -1,7 +1,10 @@
 package ariefbelajarteknologi.restful.controller;
 
+import ariefbelajarteknologi.restful.entity.User;
+import ariefbelajarteknologi.restful.model.UserResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,5 +26,14 @@ public class UserController {
     public WebResponse<String> register(@RequestBody RegisterUserRequest request) {
         userService.register(request);
         return WebResponse.<String>builder().data("OK").build();
+    }
+
+    @GetMapping(
+            path = "/api/users/current",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public WebResponse<UserResponse> get(User user){
+        UserResponse userResponse = userService.get(user);
+        return WebResponse.<UserResponse>builder().data(userResponse).build();
     }
 }
