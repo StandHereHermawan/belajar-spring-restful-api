cc3381a5095ebbccaddf501b0c17be2e72256049
StandHereHermawan <nolifegansaya@gmail.com>
2023-10-03 09:10:52 +0700

    Get User API, modify service class

--- src/main/java/ariefbelajarteknologi/restful/service/UserService.java
+++ src/main/java/ariefbelajarteknologi/restful/service/UserService.java
@@ -2,6 +2,7 @@ package ariefbelajarteknologi.restful.service;
 
 import ariefbelajarteknologi.restful.entity.User;
 import ariefbelajarteknologi.restful.model.RegisterUserRequest;
+import ariefbelajarteknologi.restful.model.UserResponse;
 import ariefbelajarteknologi.restful.repository.UserRepository;
 import ariefbelajarteknologi.restful.security.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,5 +34,12 @@ public class UserService {
         user.setName(request.getName());
 
         userRepository.save(user);
+    }
+
+    public UserResponse get(User user){
+        return UserResponse.builder()
+                .username(user.getUsername())
+                .name(user.getName())
+                .build();
     }
 }
