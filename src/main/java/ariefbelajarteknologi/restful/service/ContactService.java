6cf66b752eec88f9a69bc77f99abb011533224a2
StandHereHermawan <nolifegansaya@gmail.com>
2023-10-07 18:43:33 +0700

    Create Contact API, add service class

---
+++ src/main/java/ariefbelajarteknologi/restful/service/ContactService.java
@@ -0,0 +1,45 @@
+package ariefbelajarteknologi.restful.service;
+
+import ariefbelajarteknologi.restful.entity.Contact;
+import ariefbelajarteknologi.restful.entity.User;
+import ariefbelajarteknologi.restful.model.ContactResponse;
+import ariefbelajarteknologi.restful.model.CreateContactRequest;
+import ariefbelajarteknologi.restful.repository.ContactRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+@Service
+public class ContactService {
+
+    @Autowired
+    private ContactRepository contactRepository;
+
+    @Autowired
+    private ValidationService validationService;
+
+    @Transactional
+    public ContactResponse create(User user, CreateContactRequest request){
+        validationService.validate(request);
+
+        Contact contact = new Contact();
+        contact.setId(UUID.randomUUID().toString());
+        contact.setFirstName(request.getFirstName());
+        contact.setLastName(request.getLastName());
+        contact.setEmail(request.getEmail());
+        contact.setPhone(request.getPhone());
+        contact.setUser(user);
+
+        contactRepository.save(contact);
+
+        return ContactResponse.builder()
+                .id(contact.getId())
+                .firstName(contact.getFirstName())
+                .lastName(contact.getLastName())
+                .email(contact.getEmail())
+                .phone(contact.getPhone())
+                .build();
+    }
+}
