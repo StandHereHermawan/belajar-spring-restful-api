f5c850582fd6326b2cf3a270b59d794e814d3327
StandHereHermawan <nolifegansaya@gmail.com>
2023-10-09 16:29:44 +0700

    Get Contact API, modify service class

--- src/main/java/ariefbelajarteknologi/restful/service/ContactService.java
+++ src/main/java/ariefbelajarteknologi/restful/service/ContactService.java
@@ -6,8 +6,10 @@ import ariefbelajarteknologi.restful.model.ContactResponse;
 import ariefbelajarteknologi.restful.model.CreateContactRequest;
 import ariefbelajarteknologi.restful.repository.ContactRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.UUID;
 
@@ -34,6 +36,10 @@ public class ContactService {
 
         contactRepository.save(contact);
 
+        return toContactResponse(contact);
+    }
+
+    private ContactResponse toContactResponse(Contact contact){
         return ContactResponse.builder()
                 .id(contact.getId())
                 .firstName(contact.getFirstName())
@@ -41,5 +47,13 @@ public class ContactService {
                 .email(contact.getEmail())
                 .phone(contact.getPhone())
                 .build();
+    }
+
+    @Transactional
+    public ContactResponse get(User user,String id){
+        Contact contact = contactRepository.findFirstByUserAndId(user, id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Contact Not Found"));
+
+        return toContactResponse(contact);
     }
 }
