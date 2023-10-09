8698426aa0b3aaa998f9ddf6954257981ff45f0e
StandHereHermawan <nolifegansaya@gmail.com>
2023-10-09 16:30:06 +0700

    Get Contact API, modify repository interface

--- src/main/java/ariefbelajarteknologi/restful/repository/ContactRepository.java
+++ src/main/java/ariefbelajarteknologi/restful/repository/ContactRepository.java
@@ -1,9 +1,15 @@
 package ariefbelajarteknologi.restful.repository;
 
 import ariefbelajarteknologi.restful.entity.Contact;
+import ariefbelajarteknologi.restful.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ContactRepository extends JpaRepository<Contact, String> {
+
+    Optional<Contact> findFirstByUserAndId(User user, String id);
+
 }
