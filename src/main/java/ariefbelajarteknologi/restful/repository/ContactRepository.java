d74c6f7a8c2349bf08f1200b01c52d3ac3dcbc64
StandHereHermawan <nolifegansaya@gmail.com>
2023-10-11 16:49:56 +0700

    Search Contact API, modify repository interfaces

--- src/main/java/ariefbelajarteknologi/restful/repository/ContactRepository.java
+++ src/main/java/ariefbelajarteknologi/restful/repository/ContactRepository.java
@@ -3,12 +3,13 @@ package ariefbelajarteknologi.restful.repository;
 import ariefbelajarteknologi.restful.entity.Contact;
 import ariefbelajarteknologi.restful.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface ContactRepository extends JpaRepository<Contact, String> {
+public interface ContactRepository extends JpaRepository<Contact, String>, JpaSpecificationExecutor<Contact> {
 
     Optional<Contact> findFirstByUserAndId(User user, String id);
 
