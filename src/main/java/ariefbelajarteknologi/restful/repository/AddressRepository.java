ab3d2cc5532fee4a01dfdf0087ddf3e33fbc6949
StandHereHermawan <nolifegansaya@gmail.com>
2023-10-15 10:51:57 +0700

    Get Address API, modify repository Interface

--- src/main/java/ariefbelajarteknologi/restful/repository/AddressRepository.java
+++ src/main/java/ariefbelajarteknologi/restful/repository/AddressRepository.java
@@ -1,10 +1,14 @@
 package ariefbelajarteknologi.restful.repository;
 
 import ariefbelajarteknologi.restful.entity.Address;
+import ariefbelajarteknologi.restful.entity.Contact;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface AddressRepository extends JpaRepository<Address, String> {
 
+    Optional<Address> findFirstByContactAndId(Contact contact, String id);
 }
