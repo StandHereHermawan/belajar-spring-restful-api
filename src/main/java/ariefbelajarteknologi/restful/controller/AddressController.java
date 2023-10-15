89dab501d7e187a7b13337ae4678421790fd5273
StandHereHermawan <nolifegansaya@gmail.com>
2023-10-15 10:52:10 +0700

    Get Address API, modify controller class

--- src/main/java/ariefbelajarteknologi/restful/controller/AddressController.java
+++ src/main/java/ariefbelajarteknologi/restful/controller/AddressController.java
@@ -6,10 +6,8 @@ import ariefbelajarteknologi.restful.model.CreateAddressRequest;
 import ariefbelajarteknologi.restful.model.WebResponse;
 import ariefbelajarteknologi.restful.service.AddressService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class AddressController {
@@ -18,15 +16,30 @@ public class AddressController {
     private AddressService addressService;
 
     @PostMapping(
-            path = "/api/contacts/{contactId}/addresses"
+            path = "/api/contacts/{contactId}/addresses",
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE
     )
     public WebResponse<AddressResponse> create(
             User user,
             @RequestBody CreateAddressRequest request,
-            @PathVariable("contactId") String contactId)
-    {
+            @PathVariable("contactId") String contactId
+    ) {
         request.setContactId(contactId);
         AddressResponse addressResponse = addressService.create(user, request);
+        return WebResponse.<AddressResponse>builder().data(addressResponse).build();
+    }
+
+    @GetMapping(
+            path = "/api/contacts/{contactId}/addresses/{addressId}",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public WebResponse<AddressResponse> get(
+            User user,
+            @PathVariable("contactId") String contactId,
+            @PathVariable("addressId") String addressId
+    ) {
+        AddressResponse addressResponse = addressService.get(user, contactId, addressId);
         return WebResponse.<AddressResponse>builder().data(addressResponse).build();
     }
 }
